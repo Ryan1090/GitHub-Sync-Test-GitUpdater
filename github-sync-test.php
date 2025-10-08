80d77470390979a34dee1ed1c04837674832b057
Ryan Marrington <201040489+Ryan1090@users.noreply.github.com>
2025-10-08 08:43:39 +0800

    Update github-sync-test.php

--- github-sync-test.php
+++ github-sync-test.php
@@ -1,21 +1,8 @@
 <?php
 /**
- * Plugin Name: Github Sync Test
- * Description: test wordpress <- github sync works test x29
+ * Plugin Name: Github Sync Test GitUpdater
+ * Description: test wordpress <- github sync works test x300
  * Author: Ryan Marrington
- * GitHub Plugin URI: https://github.com/Ryan1090/GitHub-Sync-Test
  * Version: 1.0.5
  */
-
-require __DIR__ . '/vendor/autoload.php';
-use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
-
-$slug = basename(dirname(__FILE__));
-$updateChecker = PucFactory::buildUpdateChecker(
-  'https://github.com/Ryan1090/GitHub-Sync-Test',
-  __FILE__,
-  $slug,
-);
-
-$updateChecker->setBranch('main');
 
