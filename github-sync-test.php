2db54f483c164322fe9f2d0cc145b826c298ae01
Ryan Marrington <201040489+Ryan1090@users.noreply.github.com>
2025-10-08 09:04:09 +0800

    Update github-sync-test.php

--- github-sync-test.php
+++ github-sync-test.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name: Github Sync Test GitUpdater
- * Description: test wordpress <- github sync works test x200
+ * Description: test wordpress <- github sync works test x201
  * GitHub Plugin URI: https://github.com/Ryan1090/GitHub-Sync-Test-GitUpdater
  * Primary Branch: main
  * Author: Ryan Marrington
- * Version: 1.0.5
+ * Version: 1.0.6
  */
 
